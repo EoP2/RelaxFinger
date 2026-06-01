538ae0447f35e76072e53a5f8cc09f3cebaca702
EoP2 <41232498+EoP2@users.noreply.github.com>
2026-06-01 18:38:49 +0800

    Update FloatViewManager.java

--- app/src/main/java/com/hardwork/fg607/relaxfinger/manager/FloatViewManager.java
+++ app/src/main/java/com/hardwork/fg607/relaxfinger/manager/FloatViewManager.java
@@ -247,7 +247,7 @@ public class FloatViewManager implements BallView.OnBallEventListener,
         }
 */
         if (mMenuViewProxy.getMenuView().getParent() == null) {
-
+            showBackground();
             mMenuViewProxy.showMenu(mIsBallRight);
         }
 
